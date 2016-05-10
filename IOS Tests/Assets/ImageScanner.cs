0bea196dc321ab41595b65d2428e2810fa935879
Phill Tew <peatew@gmail.com>
2016-05-10 01:31:31 +0100

    Basic camera test

---
+++ IOS Tests/Assets/ImageScanner.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageScanner : MonoBehaviour
+{
+    WebCamTexture webCamTexture;
+
+    [SerializeField]
+    public bool UseWebCam = true;
+
+    [SerializeField]
+    public Texture2D TestImage;
+
+    [SerializeField]
+    public MeshRenderer CameraTest;
+
+    [SerializeField]
+    public Texture CurrentTexture;
+
+    // Use this for initialization
+    void Start()
+    {
+        webCamTexture = new WebCamTexture();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UseWebCam == true && webCamTexture.isPlaying == false)
+        {
+            webCamTexture.Play();
+        }
+        else if (UseWebCam == false && webCamTexture.isPlaying == true)
+        {
+            webCamTexture.Stop();
+        }
+
+        //if (Input.GetButtonDown("Jump") == true)
+        {
+            TryProcessImage();
+        }
+    }
+
+    private void TryProcessImage()
+    {
+        if (UseWebCam == false)
+        {
+            CurrentTexture = TestImage;
+        }
+        else
+        {
+            CurrentTexture = webCamTexture;
+        }
+
+
+        if (CameraTest != null)
+        {
+            CameraTest.material.mainTexture = CurrentTexture;
+        }
+    }
+}
