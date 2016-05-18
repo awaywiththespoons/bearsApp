8532763e20cc9bdfb2267a691495988680b25361
Phill Tew <peatew@gmail.com>
2016-05-18 02:41:48 +0100

    Basic circle test

--- IOS Tests/Assets/ImageScanner.cs
+++ IOS Tests/Assets/ImageScanner.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
 
 public class ImageScanner : MonoBehaviour
 {
+    ShapeMatch.ShapeMatcher matcher = new ShapeMatch.ShapeMatcher(); 
+
     WebCamTexture webCamTexture;
+    byte[] colorBytes = new byte[0]; 
 
     [SerializeField]
     public bool UseWebCam = true;
@@ -17,11 +22,41 @@ public class ImageScanner : MonoBehaviour
     [SerializeField]
     public Texture CurrentTexture;
 
+    [SerializeField]
+    public Text DebugText; 
+
     // Use this for initialization
     void Start()
     {
-        webCamTexture = new WebCamTexture();
+        WebCamDevice[] devices = WebCamTexture.devices;
 
+        WebCamDevice selected = default(WebCamDevice);
+
+        bool found = false;
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            Debug.Log(devices[i].name);
+
+            if (devices[i].isFrontFacing == false)
+            {
+                continue;
+            }
+
+            selected = devices[i];
+            found = true;
+        }
+
+        if (found == false && devices.Length > 0)
+        {
+            selected = devices[0];
+            found = true; 
+        }
+
+        if (found == true)
+        {
+            webCamTexture = new WebCamTexture(selected.name, 128, 128, 10);
+        }
     }
 
     // Update is called once per frame
@@ -47,16 +82,67 @@ public class ImageScanner : MonoBehaviour
         if (UseWebCam == false)
         {
             CurrentTexture = TestImage;
+
+            if (colorBytes.Length != (TestImage.width * 4) * TestImage.height)
+            {
+                colorBytes = new byte[(TestImage.width * 4) * TestImage.height]; 
+            }
+
+            Color32[] colorData = TestImage.GetPixels32();
+
+            GetArrayBytes(colorData, TestImage.width * TestImage.height, colorBytes);
+            
+            matcher.Match(CurrentTexture.width, CurrentTexture.height, colorBytes);
         }
         else
         {
             CurrentTexture = webCamTexture;
+
+            if (webCamTexture.didUpdateThisFrame == false)
+            {
+                return; 
+            }
+
+            if (colorBytes.Length != (webCamTexture.width * 4) * webCamTexture.height)
+            {
+                colorBytes = new byte[(webCamTexture.width * 4) * webCamTexture.height];
+            }
+
+            Color32[] colorData = webCamTexture.GetPixels32();
+
+            GetArrayBytes(colorData, webCamTexture.width * webCamTexture.height, colorBytes);
+
+            matcher.Match(CurrentTexture.width, CurrentTexture.height, colorBytes);
         }
 
+        if (DebugText != null)
+        {
+            DebugText.text = "Count: " + matcher.Circles.Count.ToString(); 
+        }
 
         if (CameraTest != null)
         {
             CameraTest.material.mainTexture = CurrentTexture;
         }
+    }
+
+    private static void GetArrayBytes<T>(T[] array, int count, byte[] bytes)
+    {
+        int index = 0; 
+        int size = Marshal.SizeOf(typeof(T));
+        int totalSize = size * count;
+
+        GCHandle handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+
+        try
+        {
+            Marshal.Copy(handle.AddrOfPinnedObject(), bytes, index, totalSize);
+        }
+        finally
+        {
+            handle.Free();
+        }
+
+        index += totalSize;
     }
 }
---
+++ ShapeMatch/ShapeMatch/ShapeMatcher.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Math.Geometry;
+
+namespace ShapeMatch
+{
+    public struct Circle
+    {
+        public double X, Y, Radius;
+
+        public override string ToString()
+        {
+            return string.Format("X:{0}, Y:{1}, Radius:{2}", X, Y, Radius);
+        }
+    }
+
+    public class ShapeMatcher
+    {
+        public readonly List<Circle> Circles = new List<Circle>(); 
+
+        public void Match(int width, int height, byte[] data)
+        {
+            Circles.Clear(); 
+
+            using (Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb))
+            {
+                // lock image
+                BitmapData bitmapData = bitmap.LockBits(
+                    new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                    ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+                var ptr = bitmapData.Scan0;
+
+                Marshal.Copy(data, 0, ptr, data.Length);
+
+                // step 1 - turn background to black
+                ColorFiltering colorFilter = new ColorFiltering();
+
+                colorFilter.Red = new IntRange(0, 64);
+                colorFilter.Green = new IntRange(0, 64);
+                colorFilter.Blue = new IntRange(0, 64);
+                colorFilter.FillOutsideRange = false;
+
+                colorFilter.ApplyInPlace(bitmapData);
+
+                // step 2 - locating objects
+                BlobCounter blobCounter = new BlobCounter();
+
+                blobCounter.FilterBlobs = true;
+                blobCounter.MinHeight = 5;
+                blobCounter.MinWidth = 5;
+
+                blobCounter.ProcessImage(bitmapData);
+
+                Blob[] blobs = blobCounter.GetObjectsInformation();
+
+                bitmap.UnlockBits(bitmapData);
+
+                // step 3 - check objects' type and highlight
+                SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+               
+                for (int i = 0, n = blobs.Length; i < n; i++)
+                {
+                    List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
+
+                    DoublePoint center;
+                    double radius;
+
+                    // is circle ?
+                    if (shapeChecker.IsCircle(edgePoints, out center, out radius))
+                    {
+                        Circles.Add(new Circle()
+                        {
+                            X = center.X, 
+                            Y = center.Y, 
+                            Radius = radius,
+                        }); 
+                    }
+                }
+            }
+        }
+    }
+}
