f89303f56b5d40076f039fc7c529ab54bad74d3c
Becca Rose <beckaroona@gmail.com>
2016-05-19 21:22:15 +0100

    moving accross the sreenie

---
+++ IOS Tests/Assets/Puppet2D/Scripts/Puppet2D_SortingLayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+
+
+public class Puppet2D_SortingLayer : MonoBehaviour {
+	[HideInInspector]
+	public Vector2 offsetAmount;
+	[HideInInspector]
+	public Vector2[] uvsDefault;
+	[HideInInspector]
+	public bool initialized = false;
+//	public Material[] swappedMaterials;
+//	[HideInInspector]
+//	public int swappedMaterialIndex = 0;
+//	[HideInInspector]
+//	public List<String> swappedMaterialStrings = new List<string>();
+//	public void OnValidate()
+//	{
+//		swappedMaterialStrings.Clear ();
+//		if (swappedMaterials != null) 
+//		{
+//			foreach (Material m in swappedMaterials)
+//			{
+//				if(m!=null)
+//					swappedMaterialStrings.Add (m.name);
+//			}
+//		}
+//
+//
+//	}
+}
