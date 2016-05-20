b103bd6faac846c9b75e5852dc2adb9974db0752
Becca Rose <beckaroona@gmail.com>
2016-05-20 19:42:55 +0100

    forest scence -tick

---
+++ IOS Tests/Assets/backAccrossScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backAccrossScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(Vector3.left * Time.deltaTime, Camera.main.transform);
+
+		}
+	}
+
