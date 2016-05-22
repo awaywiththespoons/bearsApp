0c9d0fc0cdf9deb452957e89c22bb99d129139f9
Becca Rose <beckaroona@gmail.com>
2016-05-22 17:36:23 +0100

    anim mcshanim

---
+++ IOS Tests/Assets/moveLEFT.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveLEFT : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(Vector3.left * (Time.deltaTime/2), Camera.main.transform);
+	
+	}
+}
