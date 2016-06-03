7f74634e83ac7ce28458e64232d12eec8f952d5d
Becca Rose <beckaroona@gmail.com>
2016-06-03 14:35:40 +0100

    getting there

--- IOS Tests/Assets/accrossScreen.cs
+++ IOS Tests/Assets/accrossScreen.cs
@@ -11,7 +11,7 @@ public class accrossScreen : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		transform.Translate(Vector3.right * Time.deltaTime, Camera.main.transform);
+		transform.Translate(Vector3.right * (Time.deltaTime/8), Camera.main.transform);
 	
 		if (transform.position.x >= 14) {
 			transform.Translate(15, 0, 0);
